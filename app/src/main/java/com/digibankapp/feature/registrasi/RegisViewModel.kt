2cc415a2f9aa7a2f5b199a8eb451382d6bd7cf42
BAGAS <anndptra658@gmail.com>
2023-10-16 14:28:59 +0700

    Validation logic register

--- app/src/main/java/com/digibankapp/feature/registrasi/RegisViewModel.kt
+++ app/src/main/java/com/digibankapp/feature/registrasi/RegisViewModel.kt
@@ -5,5 +5,27 @@ import kotlinx.coroutines.Dispatchers
 import javax.xml.validation.Validator
 
 class RegisViewModel: ViewModel() {
+    var isEmailValid = false
+    var isPasswordValid = false
+    var isNikValid = false
+    var isNohpValid = false
+    fun validateEmail(email: String): Boolean{
+        isEmailValid = email.contains("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}\$".toRegex())
+        return isEmailValid
+    }
 
+    fun validatePassword(password: String): Boolean{
+        isPasswordValid = password.contains ("^(?=.*[0-9])(?=.*[a-zA-Z])[a-zA-Z0-9]{8,}\$".toRegex())
+        return  isPasswordValid
+    }
+
+    fun validateNohp(noHp : String) : Boolean{
+        isNohpValid = noHp.length < 10
+        return isNohpValid
+    }
+
+    fun validateNik(nik : String) : Boolean{
+        isNikValid = nik.length < 16
+        return isNikValid
+    }
 }
--- app/src/main/java/com/digibankapp/feature/registrasi/Registrasi.kt
+++ app/src/main/java/com/digibankapp/feature/registrasi/Registrasi.kt
@@ -2,6 +2,7 @@ package com.digibankapp.feature.registrasi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.widget.doOnTextChanged
 import androidx.lifecycle.ViewModelProvider
 import com.digibankapp.R
 import com.digibankapp.databinding.ActivityRegistrasiBinding
@@ -15,6 +16,49 @@ class Registrasi : AppCompatActivity() {
         setContentView(binding.root)
 
         viewmodel = ViewModelProvider(this).get(RegisViewModel::class.java)
+
+        binding.apply {
+            etEmail.editText?.doOnTextChanged { text, start, before, count ->
+                if (viewmodel.validateEmail(text.toString())){
+                    binding.etEmail.isErrorEnabled = false
+                    binding.btnRegist.isEnabled = true
+                } else {
+                    binding.etEmail.isErrorEnabled = true
+                    binding.btnRegist.isEnabled = false
+                    binding.etEmail.error = "Email yang dimasukkan harus sesuai format"
+                }
+            }
+            etPassword.editText?.doOnTextChanged { text, start, before, count ->
+                if (viewmodel.validatePassword(text.toString())){
+                    binding.etPassword.isErrorEnabled = false
+                    binding.btnRegist.isEnabled = true
+                } else {
+                    binding.etPassword.isErrorEnabled = true
+                    binding.btnRegist.isEnabled = false
+                    binding.etPassword.error = "Password harus terdiri dari huruf dan angka"
+                }
+            }
+            etTelepon.editText?.doOnTextChanged { text, start, before, count ->
+                if (viewmodel.validateNohp(text.toString())){
+                    binding.etTelepon.isErrorEnabled = true
+                    binding.btnRegist.isEnabled = false
+                    binding.etTelepon.error = "Bilangan harus min. 10 karakter"
+                } else {
+                    binding.etTelepon.isErrorEnabled = false
+                    binding.btnRegist.isEnabled = true
+                }
+            }
+            etNIK.editText?.doOnTextChanged { text, start, before, count ->
+                if (viewmodel.validateNik(text.toString())){
+                    binding.etNIK.isErrorEnabled = true
+                    binding.btnRegist.isEnabled = false
+                    binding.etNIK.error = "Bilangan harus min. 16 karakter"
+                } else {
+                    binding.etNIK.isErrorEnabled = false
+                    binding.btnRegist.isEnabled = true
+                }
+            }
+        }
 
     }
 }
